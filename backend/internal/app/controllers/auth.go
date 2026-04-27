bdba643e98bb426590c9c3042f08d188593f20e7
danidaivaz <ckevinadv@gmail.com>
2026-04-28 02:47:57 +0700

    featL add fungsi login

--- backend/internal/app/controllers/auth.go
+++ backend/internal/app/controllers/auth.go
@@ -63,3 +63,43 @@ func Register(c *gin.Context) {
 		},
 	})
 }
+
+// Format data yang diharapkan saat user login
+type LoginInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+func Login(c *gin.Context) {
+	var input LoginInput
+
+	// Validasi Input
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Email dan password wajib diisi dengan benar."})
+		return
+	}
+
+	// Cari user berdasarkan email di database
+	var user models.User
+	if err := config.DB.Where("email = ?", input.Email).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Email atau password salah."})
+		return
+	}
+
+	// Bandingkan password yang diinput dengan password hash di database
+	err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(input.Password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Email atau password salah."})
+		return
+	}
+
+	// Jika sukses, berikan respon berhasil (TODO: JWT Token)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Login berhasil!",
+		"user": gin.H{
+			"id":    user.ID,
+			"name":  user.Name,
+			"email": user.Email,
+		},
+	})
+}
